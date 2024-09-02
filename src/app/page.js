19b0068a66edd004912a7d61ed72c715f4afcc2e
shamvrueth <145780447+shamvrueth@users.noreply.github.com>
2024-09-02 22:20:11 +0530

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import Navbar from "./components/Navbar";
 import Marquee from "./components/Marquee";
+import Notifs from "./components/Notifs";
 
 export default function Home() {
   return (
     <main classname="bg-green-300 p-4">
       <Navbar />
       <Marquee />
+      <Notifs cleared = {1}/>
     </main>
   );
 }
