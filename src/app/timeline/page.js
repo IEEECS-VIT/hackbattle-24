5c760768aabae3767014007e356cf0ac2d053bb6
Ansh Mehta <anshkapilmehta@gmail.com>
2024-09-04 22:17:13 +0530

    fix: added event start date to landing and fixed margin on timeline

--- src/app/timeline/page.js
+++ src/app/timeline/page.js
@@ -5,7 +5,7 @@ import { useState } from "react";
 import Image from "next/image";
 
 export default function Timeline() {
-  const [type, setType] = useState(4);
+  const [type, setType] = useState(0);
 
   const handleChangeState = (newState) => {
     if (newState >= 1 && newState <= 4) {
@@ -16,7 +16,7 @@ export default function Timeline() {
   return (
     <div className="overflow-hidden">
       <Navbar />
-      <section className="relative w-screen h-[20vh] md:h-[75vh] bg-[#5AD178] overflow-hidden">
+      <section className="mt-[8vh] md:mt-[10vh] relative w-screen h-[25vh] md:h-[75vh] bg-[#5AD178] overflow-hidden">
         {/* Dynamically use images based on the `type` state */}
         <div className="absolute inset-0 flex justify-center items-center">
           <Image
@@ -41,7 +41,7 @@ export default function Timeline() {
         />
       </section>
       {/* Text container positioned absolutely */}
-      <div className="absolute top-[calc(30vh-1rem)] md:top-[calc(80vh-1rem)] left-1/2 transform -translate-x-1/2">
+      <div className="absolute top-[calc(35vh-1rem)] md:top-[calc(80vh-1rem)] left-1/2 transform -translate-x-1/2">
         <h1 className="text-[4vh] md:text-[10vh] text-center font-bold text-customBlue font-crackman">
           TASK SUBMISSION
         </h1>
