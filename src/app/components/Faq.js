b4fbc4db71ab759729a9adc1661fa2425734f70d
Aryan Ramesh Jain <138214350+jainaryan04@users.noreply.github.com>
2024-09-08 23:34:21 +0530

    feat: add faq page

---
+++ src/app/components/Faq.js
@@ -0,0 +1,106 @@
+import React, { useState } from "react";
+import Accordion from "./Accordion.js";
+
+const HackFaqItems = [
+  {
+    title: "What is ARCS?",
+    content: (
+      <>
+        ARCS is the flagship event of IEEE-CS which has Hackbattle and Cicada as
+        its two main events. It is a platform for students to showcase their
+        technical skills and knowledge.
+      </>
+    ),
+  },
+  {
+    title: "Where can we register?",
+    content: (
+      <>
+        Registrations will be open soon, stay tuned to our social media handles
+        for updates. We will be updating the registration links on our website
+        as well.
+      </>
+    ),
+  },
+  {
+    title: "When are the events?",
+    content: (
+      <>
+        ARCS will be hosted during pre-gravitas, the dates for the same will be
+        announced soon.
+      </>
+    ),
+  },
+  {
+    title: "Where can we register?",
+    content: (
+      <>
+        Registrations will be open soon, stay tuned to our social media handles
+        for updates. We will be updating the registration links on our website
+        as well.
+      </>
+    ),
+  },
+  {
+    title: "When are the events?",
+    content: (
+      <>
+        ARCS will be hosted during pre-gravitas, the dates for the same will be
+        announced soon.
+      </>
+    ),
+  },
+  {
+    title: "Will OD's be provided to participants?",
+    content: (
+      <>
+        Yes, OD's will be provided to all participants of both the events for
+        the whole duration of the event.
+      </>
+    ),
+  },
+];
+
+const Faq = () => {
+  const [selected, setSelected] = useState("HackBattle");
+  const [activeIndex, setActiveIndex] = useState(null); // State to track the active FAQ
+
+  const handleChange = (event) => {
+    setSelected(event.target.value);
+  };
+
+  const handleAccordionClick = (index) => {
+    setActiveIndex(index === activeIndex ? null : index); // Toggle the active FAQ
+  };
+
+  const getItems = () => {
+    return HackFaqItems;
+  };
+
+  return (
+    <div className="relative font-pixeboy flex min-h-screen w-screen flex-col items-center justify-around p-2 text-2xl sm:p-4 ws:flex-row md:p-10">
+      {/* Background Image */}
+      <img
+        src="/purple.png"
+        className="absolute top-0 left-0 h-full w-full object-cover z-0"
+        alt="Background"
+      />
+
+      {/* Content in front of the background */}
+      <div className="relative z-10 flex flex-col items-center gap-4 text-center my-12">
+        <p className="text-[10vh] lg:text-[20vh]">FAQS</p>
+      </div>
+
+      {/* Accordion Component */}
+      <div className="relative z-10 flex justify-center w-[90vw] lg:w-[65vw]">
+        <Accordion
+          items={getItems()}
+          activeIndex={activeIndex}
+          onClick={handleAccordionClick}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Faq;
--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,5 @@
-
+"use client";
+import { useState } from "react";
 import Navbar from "./components/Navbar";
 import Marquee from "./components/Marquee";
 import Landing from "./components/landing";
@@ -6,18 +7,22 @@ import About from "./components/about";
 import Tracks from "./components/track";
 import Speaker from "./components/speaker";
 import Conducted from "./components/conducted";
-import FAQ from "./components/faq";
+import FAQ from "./components/Faq";
+import TeamCode from "./components/TeamCode"
 
 export default function Home() {
+  const [code,setCode]=useState("Hello");
+  const [flag,setFlag]=useState(true);
   return (
     <main className="overflow-hidden w-full">
       <Navbar />
       <Landing />
       <About />
+      {<TeamCode visible={flag} code={code}/>}
       {/* <Tracks />
       <Speaker />
-      <Conducted />
-      <FAQ /> */}
+      <Conducted /> */}
+      <FAQ /> 
     </main>
   );
 }
