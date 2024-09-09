7502a98b631b6b2d8664190f3af1b5a7cbcae444
ashmit <ashmit.dudeja@gmail.com>
2024-09-09 23:08:39 +0530

    added cards

--- src/app/page.js
+++ src/app/page.js
@@ -10,14 +10,15 @@ import Tracks from "./components/track";
 import Speaker from "./components/speaker";
 import Conducted from "./components/conducted";
 import FAQ from "./components/Faq";
-import TeamCode from "./components/TeamCode"
+
+
 
 export default function Home() {
   const [code,setCode]=useState("Hello");
   const [flag,setFlag]=useState(true);
   return (
     <main className="overflow-hidden w-full">
-      {/* <Navbar /> */}
+       <Navbar /> 
       <Landing />
       <About />
       {<TeamCode visible={flag} code={code}/>}
