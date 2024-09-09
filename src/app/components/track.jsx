2a37c86ef914f413ba6cdcac2c06508da25f87d7
Tithi Shah <shahtithi21@gmail.com>
2024-09-09 19:52:19 +0530

    tracks page updated

--- src/app/components/track.jsx
+++ src/app/components/track.jsx
@@ -2,91 +2,101 @@ import TeamCode from "./Teamcode";
 
 export default function Tracks() {
   return (
-    <section className=" relative h-[100vh]  bg-[#FFC642] flex items-center justify-center">
-      <img
+    <section className="relative h-[100vh]  bg-[#FFC642] flex items-center justify-center">
+      {/* <img
         src="trackpixel.svg"
         alt="Overlay Image"
         className="absolute w-full h-full object-cover"
-      />
-      <img
+      /> */}
+      {/* <img
         src="pagedown.svg"
         alt="pagedown"
         className="absolute h-full  w-screen object-contain z-0"
-      />
-      <p className="bg-white p-4 text-5xl border-black border-2 w-[30vh] h-[15vh] mr-[5%] text-center font-pixeboy mb-[20px]">
+      /> */}
+      <p className="bg-[#FFC642] pt-2 text-8xl w-[50vw] h-[10vh] mr-[0.3%] text-center font-pixeboy mb-[88vh]">
         TRACKS
-      </p>
+      </p> 
 
-      <div className="w-[15vw] h-[25vh] lg:h-[30vh] lg:w-[17vw] bg-blue-600 border-black border-4 shadow-[-5px_5px_0_0_#644997] lg:shadow-[-10px_10px_0_0_#644997] z-10 absolute top-[5%] left-[25%]">
-        <div className="bg-[#FFF] h-[2vh] lg:h-[3vh] flex items-center border-black border-b-4 pl-[0.5vw]">
+      <div className="w-[25vw] h-[35vh]  sm:w-[28vw] sm:h-[35vh] lg:h-[35vh] lg:w-[25vw] sm:ml-0.1rem bg-blue-600 border-black border-4 shadow-[-5px_5px_0_0_#644997] lg:shadow-[-10px_10px_0_0_#644997] z-10 absolute top-[18%] left-[10%] ">
+        <div className="bg-[#FFF] h-[4vh] lg:h-[5vh] flex items-center border-black border-b-4 pl-[0.5vw]">
           <img
             src="/yellow-circles.svg"
             alt="Yellow Circles"
-            className="h-[1vh] lg:h-[2vh]"
+            className="h-[2vh] lg:h-[3vh]"
           />
         </div>
 
-        <div className="flex flex-col items-center h-[38vh] font-pixeboy text-[3vh] lg:text-[5vh]">
-          <img src="./fluent-mdl2_robot.svg" alt="" className="h-[40%] mb-2" />
+        <div className="flex flex-col items-center h-[45vh] font-pixeboy text-[3vh] lg:text-[5vh]">
+          <img src="./fluent-mdl2_robot.svg" alt="" className="h-[50%] mb-2" />
           <p className="text-5xl text-yellow-500">AI/ML</p>
         </div>
       </div>
-      <div className="w-[15vw] h-[25vh] lg:h-[30vh] lg:w-[17vw] bg-blue-600 border-black border-4 shadow-[-5px_5px_0_0_#644997] lg:shadow-[-10px_10px_0_0_#644997] z-10 absolute top-[5%] left-[55%]">
-        <div className="bg-[#FFF] h-[2vh] lg:h-[3vh] flex items-center border-black border-b-4 pl-[0.5vw]">
+
+
+      <div className="w-[25vw] h-[35vh] sm:w-[28vw] sm:h-[35vh] lg:h-[35vh] lg:w-[25vw] bg-blue-600 border-black border-4 shadow-[-5px_5px_0_0_#644997] lg:shadow-[-10px_10px_0_0_#644997] z-10 absolute top-[18%] left-[40%]">
+        <div className="bg-[#FFF] h-[4vh] lg:h-[5vh] flex items-center border-black border-b-4 pl-[0.5vw]">
           <img
             src="/yellow-circles.svg"
             alt="Yellow Circles"
-            className="h-[1vh] lg:h-[2vh]"
+            className="h-[2vh] lg:h-[3vh]"
           />
         </div>
-
-        <div className="flex flex-col items-center h-[38vh] font-pixeboy text-[3vh] lg:text-[5vh]">
-          <img src="./fluent-mdl2_robot.svg" alt="" className="h-[40%] mb-2" />
+      
+        <div className="flex flex-col items-center h-[45vh] font-pixeboy text-[3vh] lg:text-[5vh]">
+          <img src="./fluent-mdl2_robot.svg" alt="" className="h-[50%] mb-2" />
           <p className="text-5xl text-yellow-500">AI/ML</p>
         </div>
       </div>
-      <div className="w-[15vw] h-[25vh] lg:h-[30vh] lg:w-[17vw] bg-blue-600 border-black border-4 shadow-[-5px_5px_0_0_#644997] lg:shadow-[-10px_10px_0_0_#644997] z-10 absolute top-[40%] right-[20%]">
-        <div className="bg-[#FFF] h-[2vh] lg:h-[3vh] flex items-center border-black border-b-4 pl-[0.5vw]">
+
+
+      <div className="w-[25vw] h-[35vh] sm:w-[28vw] sm:h-[35vh] lg:h-[35vh] lg:w-[25vw] bg-blue-600 border-black border-4 shadow-[-5px_5px_0_0_#644997] lg:shadow-[-10px_10px_0_0_#644997] z-10 absolute top-[18%] left-[70%]">
+        <div className="bg-[#FFF] h-[4vh] lg:h-[5vh] flex items-center border-black border-b-4 pl-[0.5vw]">
           <img
             src="/yellow-circles.svg"
             alt="Yellow Circles"
-            className="h-[1vh] lg:h-[2vh]"
+            className="h-[2vh] lg:h-[3vh]"
           />
         </div>
 
-        <div className="flex flex-col items-center h-[38vh] font-pixeboy text-[3vh] lg:text-[5vh]">
-          <img src="./fluent-mdl2_robot.svg" alt="" className="h-[40%] mb-2" />
+        <div className="flex flex-col items-center h-[40vh] font-pixeboy text-[3vh] lg:text-[5vh]">
+          <img src="./fluent-mdl2_robot.svg" alt="" className="h-[50%] mb-2" />
           <p className="text-5xl text-yellow-500">AI/ML</p>
         </div>
       </div>
-      <div className="w-[15vw] h-[25vh] lg:h-[30vh] lg:w-[17vw] bg-blue-600 border-black border-4 shadow-[-5px_5px_0_0_#644997] lg:shadow-[-10px_10px_0_0_#644997] z-10 absolute top-[45%] left-[15%]">
-        <div className="bg-[#FFF] h-[2vh] lg:h-[3vh] flex items-center border-black border-b-4 pl-[0.5vw]">
+
+
+      <div className="w-[25vw] h-[35vh] sm:w-[28vw] sm:h-[35vh] lg:h-[35vh] lg:w-[25vw] bg-blue-600 border-black border-4 shadow-[-5px_5px_0_0_#644997] lg:shadow-[-10px_10px_0_0_#644997] z-10 absolute top-[60%] left-[25%]">
+        <div className="bg-[#FFF] h-[4vh] lg:h-[5vh] flex items-center border-black border-b-4 pl-[0.5vw]">
           <img
             src="/yellow-circles.svg"
             alt="Yellow Circles"
-            className="h-[1vh] lg:h-[2vh]"
+            className="h-[2vh] lg:h-[3vh]"
           />
         </div>
 
-        <div className="flex flex-col items-center h-[38vh] font-pixeboy text-[3vh] lg:text-[5vh]">
-          <img src="./fluent-mdl2_robot.svg" alt="" className="h-[40%] mb-2" />
+        <div className="flex flex-col items-center h-[40vh] font-pixeboy text-[3vh] lg:text-[5vh]">
+          <img src="./fluent-mdl2_robot.svg" alt="" className="h-[50%] mb-2" />
           <p className="text-5xl text-yellow-500">AI/ML</p>
         </div>
       </div>
-      <div className="w-[15vw] h-[25vh] lg:h-[30vh] lg:w-[17vw] bg-blue-600 border-black border-4 shadow-[-5px_5px_0_0_#644997] lg:shadow-[-10px_10px_0_0_#644997] z-10 absolute bottom-[9%] left-[37%]">
-        <div className="bg-[#FFF] h-[2vh] lg:h-[3vh] flex items-center border-black border-b-4 pl-[0.5vw]">
+
+
+      <div className="w-[25vw] h-[35vh] sm:w-[28vw] sm:h-[35vh] lg:h-[35vh] lg:w-[25vw] bg-blue-600 border-black border-4 shadow-[-5px_5px_0_0_#644997] lg:shadow-[-10px_10px_0_0_#644997] z-10 absolute top-[60%] left-[55%]">
+        <div className="bg-[#FFF] h-[4vh] lg:h-[5vh] flex items-center border-black border-b-4 pl-[0.5vw]">
           <img
             src="/yellow-circles.svg"
             alt="Yellow Circles"
-            className="h-[1vh] lg:h-[2vh]"
+            className="h-[2vh] lg:h-[3vh]"
           />
         </div>
 
-        <div className="flex flex-col items-center h-[38vh] font-pixeboy text-[3vh] lg:text-[5vh]">
-          <img src="./fluent-mdl2_robot.svg" alt="" className="h-[40%] mb-2" />
+        <div className="flex flex-col items-center h-[40vh] font-pixeboy text-[3vh] lg:text-[5vh]">
+          <img src="./fluent-mdl2_robot.svg" alt="" className="h-[50%] mb-2" />
           <p className="text-5xl text-yellow-500">AI/ML</p>
         </div>
       </div>
+
+      
     </section>
   );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,4 @@
-
+import track from "./components/track";
 import Navbar from "./components/Navbar";
 import Marquee from "./components/Marquee";
 import Landing from "./components/landing";
@@ -8,14 +8,15 @@ import Speaker from "./components/speaker";
 import Conducted from "./components/conducted";
 import FAQ from "./components/faq";
 
+
 export default function Home() {
   return (
     <main className="overflow-hidden w-full">
       <Navbar />
       <Landing />
       <About />
-      {/* <Tracks />
-      <Speaker />
+      <Tracks />
+      {/*<Speaker />
       <Conducted />
       <FAQ /> */}
     </main>
