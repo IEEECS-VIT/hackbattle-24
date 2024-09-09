750e6ae3d91d8e6c2d91530a46c178557d8408e5
Tithi Shah <shahtithi21@gmail.com>
2024-09-09 23:11:11 +0530

    single card og track page made

--- src/app/components/track.jsx
+++ src/app/components/track.jsx
@@ -2,101 +2,69 @@ import TeamCode from "./Teamcode";
 
 export default function Tracks() {
   return (
-    <section className="relative h-[100vh]  bg-[#FFC642] flex items-center justify-center">
-      {/* <img
-        src="trackpixel.svg"
-        alt="Overlay Image"
-        className="absolute w-full h-full object-cover"
-      /> */}
-      {/* <img
-        src="pagedown.svg"
-        alt="pagedown"
-        className="absolute h-full  w-screen object-contain z-0"
-      /> */}
-      <p className="bg-[#FFC642] pt-2 text-8xl w-[50vw] h-[10vh] mr-[0.3%] text-center font-pixeboy mb-[88vh]">
+    <section className="relative min-h-[100vh] bg-[#FFC642] flex flex-col items-center justify-center">
+      {/* Title */}
+      <p className="bg-[#FFC642] pt-2 text-6xl w-full sm:w-[50vw] h-[10vh] text-center font-pixeboy mt-3 mb-10">
         TRACKS
-      </p> 
+      </p>
 
-      <div className="w-[25vw] h-[35vh]  sm:w-[28vw] sm:h-[35vh] lg:h-[35vh] lg:w-[25vw] sm:ml-0.1rem bg-blue-600 border-black border-4 shadow-[-5px_5px_0_0_#644997] lg:shadow-[-10px_10px_0_0_#644997] z-10 absolute top-[18%] left-[10%] ">
-        <div className="bg-[#FFF] h-[4vh] lg:h-[5vh] flex items-center border-black border-b-4 pl-[0.5vw]">
-          <img
-            src="/yellow-circles.svg"
-            alt="Yellow Circles"
-            className="h-[2vh] lg:h-[3vh]"
-          />
+      {/* Container for the cards */}
+      <div className="w-full flex flex-wrap justify-center  gap-8 p-4">
+        {/* Card 1 */}
+        <div className="flex flex-col items-center bg-blue-600 border-black border-4 shadow-[-5px_5px_0_0_#644997] lg:shadow-[-10px_10px_0_0_#644997]  sm:w-[45%] md:w-[30%] lg:w-[30%]  h-[75%] w-[550px]">
+          <div className="bg-[#FFF] w-full h-[4vh] lg:h-[5vh] flex items-center border-black border-b-4 pl-[0.5vw]">
+            <img src="/yellow-circles.svg" alt="Yellow Circles" className="h-[2vh] lg:h-[3vh]" />
+          </div>
+          <div className="flex flex-col items-center w-full h-[45vh] font-pixeboy text-[3vh] lg:text-[5vh]">
+            <img src="./fluent-mdl2_robot.svg" alt="" className="h-[50%] mb-2" />
+            <p className="text-5xl text-yellow-500">AI/ML</p>
+          </div>
         </div>
 
-        <div className="flex flex-col items-center h-[45vh] font-pixeboy text-[3vh] lg:text-[5vh]">
-          <img src="./fluent-mdl2_robot.svg" alt="" className="h-[50%] mb-2" />
-          <p className="text-5xl text-yellow-500">AI/ML</p>
-        </div>
-      </div>
-
-
-      <div className="w-[25vw] h-[35vh] sm:w-[28vw] sm:h-[35vh] lg:h-[35vh] lg:w-[25vw] bg-blue-600 border-black border-4 shadow-[-5px_5px_0_0_#644997] lg:shadow-[-10px_10px_0_0_#644997] z-10 absolute top-[18%] left-[40%]">
-        <div className="bg-[#FFF] h-[4vh] lg:h-[5vh] flex items-center border-black border-b-4 pl-[0.5vw]">
-          <img
-            src="/yellow-circles.svg"
-            alt="Yellow Circles"
-            className="h-[2vh] lg:h-[3vh]"
-          />
-        </div>
-      
-        <div className="flex flex-col items-center h-[45vh] font-pixeboy text-[3vh] lg:text-[5vh]">
-          <img src="./fluent-mdl2_robot.svg" alt="" className="h-[50%] mb-2" />
-          <p className="text-5xl text-yellow-500">AI/ML</p>
-        </div>
-      </div>
-
-
-      <div className="w-[25vw] h-[35vh] sm:w-[28vw] sm:h-[35vh] lg:h-[35vh] lg:w-[25vw] bg-blue-600 border-black border-4 shadow-[-5px_5px_0_0_#644997] lg:shadow-[-10px_10px_0_0_#644997] z-10 absolute top-[18%] left-[70%]">
-        <div className="bg-[#FFF] h-[4vh] lg:h-[5vh] flex items-center border-black border-b-4 pl-[0.5vw]">
-          <img
-            src="/yellow-circles.svg"
-            alt="Yellow Circles"
-            className="h-[2vh] lg:h-[3vh]"
-          />
+        {/* Card 2 */}
+        <div className="flex flex-col items-center bg-blue-600 border-black border-4 shadow-[-5px_5px_0_0_#644997] lg:shadow-[-10px_10px_0_0_#644997]  sm:w-[45%] md:w-[30%] lg:w-[30%] h-[75%] w-[550px]">
+          <div className="bg-[#FFF] w-full h-[4vh] lg:h-[5vh] flex items-center border-black border-b-4 pl-[0.5vw]">
+            <img src="/yellow-circles.svg" alt="Yellow Circles" className="h-[2vh] lg:h-[3vh]" />
+          </div>
+          <div className="flex flex-col items-center w-full h-[45vh] font-pixeboy text-[3vh] lg:text-[5vh]">
+            <img src="./fluent-mdl2_robot.svg" alt="" className="h-[50%] mb-2" />
+            <p className="text-5xl text-yellow-500">AI/ML</p>
+          </div>
         </div>
 
-        <div className="flex flex-col items-center h-[40vh] font-pixeboy text-[3vh] lg:text-[5vh]">
-          <img src="./fluent-mdl2_robot.svg" alt="" className="h-[50%] mb-2" />
-          <p className="text-5xl text-yellow-500">AI/ML</p>
-        </div>
-      </div>
-
-
-      <div className="w-[25vw] h-[35vh] sm:w-[28vw] sm:h-[35vh] lg:h-[35vh] lg:w-[25vw] bg-blue-600 border-black border-4 shadow-[-5px_5px_0_0_#644997] lg:shadow-[-10px_10px_0_0_#644997] z-10 absolute top-[60%] left-[25%]">
-        <div className="bg-[#FFF] h-[4vh] lg:h-[5vh] flex items-center border-black border-b-4 pl-[0.5vw]">
-          <img
-            src="/yellow-circles.svg"
-            alt="Yellow Circles"
-            className="h-[2vh] lg:h-[3vh]"
-          />
+        {/* Card 3 */}
+        <div className="flex flex-col items-center bg-blue-600 border-black border-4 shadow-[-5px_5px_0_0_#644997] lg:shadow-[-10px_10px_0_0_#644997]  sm:w-[45%] md:w-[30%] lg:w-[30%] h-[75%] w-[550px]">
+          <div className="bg-[#FFF] w-full h-[4vh] lg:h-[5vh] flex items-center border-black border-b-4 pl-[0.5vw]">
+            <img src="/yellow-circles.svg" alt="Yellow Circles" className="h-[2vh] lg:h-[3vh]" />
+          </div>
+          <div className="flex flex-col items-center w-full h-[45vh] font-pixeboy text-[3vh] lg:text-[5vh]">
+            <img src="./fluent-mdl2_robot.svg" alt="" className="h-[50%] mb-2" />
+            <p className="text-5xl text-yellow-500">AI/ML</p>
+          </div>
         </div>
 
-        <div className="flex flex-col items-center h-[40vh] font-pixeboy text-[3vh] lg:text-[5vh]">
-          <img src="./fluent-mdl2_robot.svg" alt="" className="h-[50%] mb-2" />
-          <p className="text-5xl text-yellow-500">AI/ML</p>
+        {/* Card 4 */}
+        <div className="flex flex-col items-center bg-blue-600 border-black border-4 shadow-[-5px_5px_0_0_#644997] lg:shadow-[-10px_10px_0_0_#644997]  sm:w-[45%] md:w-[30%] lg:w-[30%] h-[75%] w-[550px]">
+          <div className="bg-[#FFF] w-full h-[4vh] lg:h-[5vh] flex items-center border-black border-b-4 pl-[0.5vw]">
+            <img src="/yellow-circles.svg" alt="Yellow Circles" className="h-[2vh] lg:h-[3vh]" />
+          </div>
+          <div className="flex flex-col items-center w-full h-[45vh] font-pixeboy text-[3vh] lg:text-[5vh]">
+            <img src="./fluent-mdl2_robot.svg" alt="" className="h-[50%] mb-2" />
+            <p className="text-5xl text-yellow-500">AI/ML</p>
+          </div>
+        </div>
+
+        {/* Card 5 */}
+        <div className="flex flex-col items-center bg-blue-600 border-black border-4 shadow-[-5px_5px_0_0_#644997] lg:shadow-[-10px_10px_0_0_#644997] sm:w-[45%] md:w-[30%] lg:w-[30%] h-[75%] w-[550px]">
+          <div className="bg-[#FFF] w-full h-[4vh] lg:h-[5vh] flex items-center border-black border-b-4 pl-[0.5vw]">
+            <img src="/yellow-circles.svg" alt="Yellow Circles" className="h-[2vh] lg:h-[3vh]" />
+          </div>
+          <div className="flex flex-col items-center w-full h-[45vh] font-pixeboy text-[3vh] lg:text-[5vh]">
+            <img src="./fluent-mdl2_robot.svg" alt="" className="h-[50%] mb-2" />
+            <p className="text-5xl text-yellow-500">AI/ML</p>
+          </div>
         </div>
       </div>
-
-
-      <div className="w-[25vw] h-[35vh] sm:w-[28vw] sm:h-[35vh] lg:h-[35vh] lg:w-[25vw] bg-blue-600 border-black border-4 shadow-[-5px_5px_0_0_#644997] lg:shadow-[-10px_10px_0_0_#644997] z-10 absolute top-[60%] left-[55%]">
-        <div className="bg-[#FFF] h-[4vh] lg:h-[5vh] flex items-center border-black border-b-4 pl-[0.5vw]">
-          <img
-            src="/yellow-circles.svg"
-            alt="Yellow Circles"
-            className="h-[2vh] lg:h-[3vh]"
-          />
-        </div>
-
-        <div className="flex flex-col items-center h-[40vh] font-pixeboy text-[3vh] lg:text-[5vh]">
-          <img src="./fluent-mdl2_robot.svg" alt="" className="h-[50%] mb-2" />
-          <p className="text-5xl text-yellow-500">AI/ML</p>
-        </div>
-      </div>
-
-      
     </section>
   );
 }
