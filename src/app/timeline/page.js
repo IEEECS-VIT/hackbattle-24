17029a84ecc22fcdce2b8543f249aad1959eae68
Aryan Ramesh Jain <138214350+jainaryan04@users.noreply.github.com>
2024-09-16 23:13:33 +0530

    fix: timeline for mmobile

--- src/app/timeline/page.js
+++ src/app/timeline/page.js
@@ -17,9 +17,10 @@ export default function Timeline() {
   return (
     <div className="overflow-hidden ">
       <Navbar />
-      <section className="relative w-screen h-[30vh] md:h-[75vh] bg-[#5AD178] overflow-hidden">
+      <section className="relative w-screen h-[60vh] md:h-[85vh] bg-[#5AD178] overflow-hidden">
         {/* Dynamically use images based on the `type` state */}
-        <div className="absolute inset-0 flex justify-center items-center mt-[10vh]">
+        <div className="hidden lg:block" > 
+        <div className="absolute inset-0 flex justify-center items-center mt-[20vh]">
           <Image
             src={`state${type}timeline.svg`}
             alt="state image"
@@ -33,6 +34,18 @@ export default function Timeline() {
           alt="Overlay Image"
           className="absolute inset-0 w-full h-full object-cover opacity-70"
         />
+        </div>
+        <div className="w-full h-[60vh] block lg:hidden">
+        <div className="absolute inset-0 flex justify-center items-center mt-[10vh]">
+        <Image
+            src={`state${type}timelinemobile.svg`}
+            alt="state image"
+            width={1200} // Adjust width to make the image smaller
+            height={300} // Adjust height accordingly
+            className="object-contain"
+          />
+          </div>
+        </div>
       </section>
       <section className="relative w-screen h-[110vh] md:h-[160vh] lg:h-[222vh] xl:h-[150vh]  bg-[#FEC46F] overflow-hidden">
         <img
@@ -42,7 +55,7 @@ export default function Timeline() {
         />
       </section>
       {/* Text container positioned absolutely */}
-      <div className="absolute top-[calc(30vh-1rem)] md:top-[calc(80vh-1rem)] left-1/2 transform -translate-x-1/2">
+      <div className="absolute top-[calc(65vh-1rem)] md:top-[calc(90vh-1rem)] left-1/2 transform -translate-x-1/2">
         <h1 className="text-[4vh] md:text-[10vh] text-center font-bold text-customBlue font-crackman">
           TASK SUBMISSION
         </h1>
