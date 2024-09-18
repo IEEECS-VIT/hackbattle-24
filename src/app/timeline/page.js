4b91703e39ec7d942d4b4f8f5c0b786a8b9b4ad9
Aryan Ramesh Jain <138214350+jainaryan04@users.noreply.github.com>
2024-09-18 22:52:59 +0530

    update: timeline - stretch to fill screen

--- src/app/timeline/page.js
+++ src/app/timeline/page.js
@@ -17,7 +17,7 @@ export default function Timeline() {
   return (
     <div className="overflow-hidden ">
       <Navbar />
-      <section className="relative w-screen h-[60vh] md:h-[85vh] bg-[#5AD178] overflow-hidden">
+      <section className="relative w-screen h-[100vh] md:h-[100vh] bg-[#5AD178] overflow-hidden">
         {/* Dynamically use images based on the `type` state */}
         <div className="hidden lg:block" > 
         <div className="absolute inset-0 flex justify-center items-center mt-[20vh]">
@@ -52,20 +52,7 @@ export default function Timeline() {
         />
         </div>
       </section>
-      <section className="relative w-screen h-[110vh] md:h-[160vh] lg:h-[222vh] xl:h-[150vh]  bg-[#FEC46F] overflow-hidden">
-        <img
-          src="pixelated-filter.svg"
-          alt="Overlay Image"
-          className="absolute inset-0 w-full h-full object-cover opacity-70"
-        />
-      </section>
-      {/* Text container positioned absolutely */}
-      <div className="absolute top-[calc(65vh-1rem)] md:top-[calc(90vh-1rem)] left-1/2 transform -translate-x-1/2">
-        <h1 className="text-[4vh] md:text-[10vh] text-center font-bold text-customBlue font-crackman">
-          TASK SUBMISSION
-        </h1>
-        <Card></Card>
-      </div>
+      
     </div>
   );
 }
