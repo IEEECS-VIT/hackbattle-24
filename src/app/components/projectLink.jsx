1dc6b35c5c7715e5bc381543916108ed2c706cd7
ShubhamPrasad3012 <shubhamprasadclass10@gmail.com>
2024-09-24 21:57:20 +0530

    removed breaking

--- src/app/components/projectLink.jsx
+++ src/app/components/projectLink.jsx
@@ -2,7 +2,7 @@ export default function ProjectLink() {
     return (
         <div className="h-[30vh] shadow-[15px_15px_0_0_#644997] md:h-[40vh] w-[80vw] md:w-[40vw] lg:w-[30vw] border-2 border-black rounded-lg bg-red-600 relative  overflow-y-auto flex flex-col">
             <div className="flex justify-between items-center bg-yellow-300 p-2 border-b-2 border-pink-300">
-                <div className="text-purple-500 font-pixeboy text-[3.8vh] md:text-[4.8vh]">{`<PROJECT LINKS>`}</div>
+                <div className="text-purple-500 font-pixeboy text-[3.8vh] md:text-[3.8vh]">{`<PROJECT LINKS>`}</div>
                 <div className="flex">
                     <div className="flex space-x-2">
                         <div className="w-3 h-3 border-2 border-purple-500 rounded-full"></div>
